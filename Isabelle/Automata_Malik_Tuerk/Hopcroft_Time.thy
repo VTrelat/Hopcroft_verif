b8250cc526be0efd730d0c77ecce98ee75cf6422
Vincent Trélat <vincent.trelat@free.fr>
2023-04-25 12:12:36 +0200

    Update Hopcroft_Time.thy

--- Isabelle/Automata_Malik_Tuerk/Hopcroft_Time.thy
+++ Isabelle/Automata_Malik_Tuerk/Hopcroft_Time.thy
@@ -4,4 +4,39 @@ theory Hopcroft_Time
   imports Hopcroft_Minimisation
 begin
 
+text
+\<open>
+  Abstract level I
+    def: Hopcroft_abstract
+\<close>
+term Hopcroft_abstract
+
+text
+\<open>
+  Abstract level II
+  Refinement of the specification for acquiring next state toward an inner loop.
+    def: Hopcroft_set_f
+\<close>
+
+text
+\<open>
+  Abstract level III
+  Precomputation of the set of predecessors of the currently chosen set.
+    def: Hopcroft_precompute_step
+\<close>
+
+text
+\<open>
+  First data refinement
+  Refinement towards efficient data structures. Partition of \<Q> \<rightarrow> maps
+    def: Hopcroft_map
+\<close>
+
+text
+\<open>
+  Second data refinement
+  Classes as sets \<rightarrow> maps (bijection with natural numbers).
+    def: Hopcroft_map2
+\<close>
+
 end
