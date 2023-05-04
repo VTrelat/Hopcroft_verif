231a5b35dfeb8ee2edc8e28936dc48f0aa798bec
Vincent Trélat <vincent.trelat@free.fr>
2023-05-04 12:12:58 +0200

    Experimenting with NREST

--- Isabelle/Automata_Malik_Tuerk/Hopcroft_Time.thy
+++ Isabelle/Automata_Malik_Tuerk/Hopcroft_Time.thy
@@ -2,8 +2,9 @@ section \<open>Running-time analysis of Hopcroft's algorithm\<close>
 
 theory Hopcroft_Time
   imports
-    Hopcroft_Minimisation
-    "../isabelle_llvm_time/thys/lib/More_Asymptotics"
+    (* Hopcroft_Minimisation *) (* Errors due to duplicate theories? Should we merge things?*)
+    "../isabelle_llvm_time/thys/nrest/NREST_Main"
+    "../isabelle_llvm_time/thys/vcg/Sep_Generic_Wp"
 begin
 
 text
@@ -11,7 +12,12 @@ text
   Abstract level I
     def: Hopcroft_abstract
 \<close>
-thm Hopcroft_abstract_def
+(* thm Hopcroft_abstract_def *)
+(* thm Hopcroft_abstract_f_def *)
+
+definition "state_emptiness \<equiv> undefined"
+
+definition "line1 \<equiv> SPECT[(\<lambda>Q. Q = {}) \<mapsto> cost ($state_emptiness)]"
 
 text
 \<open>
@@ -19,7 +25,7 @@ text
   Refinement of the specification for acquiring next state toward an inner loop.
     def: Hopcroft_set_f
 \<close>
-thm Hopcroft_set_f_def
+(* thm Hopcroft_set_f_def *)
 
 text
 \<open>
@@ -27,7 +33,7 @@ text
   Precomputation of the set of predecessors of the currently chosen set.
     def: Hopcroft_precompute_step
 \<close>
-thm Hopcroft_precompute_step_def
+(* thm Hopcroft_precompute_step_def *)
 
 text
 \<open>
@@ -35,7 +41,7 @@ text
   Refinement towards efficient data structures. Partition of \<Q> \<rightarrow> maps
     def: Hopcroft_map
 \<close>
-thm Hopcroft_map_def
+(* thm Hopcroft_map_def *)
 
 text
 \<open>
@@ -43,27 +49,15 @@ text
   Classes as sets \<rightarrow> maps (bijection with natural numbers).
     def: Hopcroft_map2
 \<close>
-thm Hopcroft_map2_def
+(* thm Hopcroft_map2_def *)
 
 text
 \<open>
   Implementation
   Instantiation of the locales
 \<close>
-thm hopcroft_impl_def
-thm hop_impl.Hopcroft_code_def
+(* thm hopcroft_impl_def *)
+(* thm hop_impl.Hopcroft_code_def *)
 
-definition
-  "hopcroft_impl_allcost m n \<equiv> m * n * Discrete.log n"
-
-theorem "(\<lambda>(m, n). real (hopcroft_impl_allcost m n)) \<in> O(\<lambda>(m,n). real (m * n) * ln (real n))"
-proof-
-  have "(\<lambda>n. real (m * n * Discrete.log n)) \<in> O(\<lambda>n. real (m * n) *ln (real n))" for m
-    using dlog by fastforce
-  show ?thesis
-    unfolding hopcroft_impl_allcost_def
-    apply standard
-    apply auto sorry
-qed
 
 end
